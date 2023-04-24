7d4d6607a6d70964eeaed252717bb82941ca0a82
lcqff <71930280+lcqff@users.noreply.github.com>
2023-04-24 21:42:23 +0900

    feat: Done fizzbuzz
    Print fizzbuzz if i is multiple if 15.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,9 +8,11 @@ def do_fizzbuzz(num:int):
     """
     
     for i in range(1,num+1):
-        if i%3==0:
+        if i%15==0:
+            print("fizzbuzz")
+        elif i%3==0:
             print("fizz")
-        elif i%5: 
+        elif i%5==0: 
             print("buzz")
         else:
             print(f'{i}')
